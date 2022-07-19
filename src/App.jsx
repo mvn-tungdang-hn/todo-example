cf8147dafcfe082b856d8c17fd87da694c12e033
mvn-tungdang-hn <tung.dang@monstar-lab.com>
2022-07-19 15:42:23 +0700

    create feature todo

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import TodoListScreen from "features/todo/screens/TodoListScreen/TodoListScreen";
+
+const App = () => {
+  return (
+    <div>
+      <TodoListScreen />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/features/todo/screens/TodoListScreen/TodoListScreen.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TodoListScreen = () => {
+  return <div>TodoListScreen</div>;
+};
+
+export default TodoListScreen;
